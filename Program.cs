acc28592f5654dda77cb8d61b09d92e0b1b1ef9a
evtus <evtusenkodana@gmail.com>
2025-05-18 21:10:56 +0300

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace CharMatrix;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8; // Це для того, щоб консоль мала змогу бачити Українські букви
+        char[,] myMatrix = {
+            { 'а', 'б', 'в' },
+            { 'г', 'е', 'д' },
+            { 'є', 'ж', 'і' }
+        };
+
+        CharMatrix matrix = new CharMatrix(myMatrix);
+
+        Console.WriteLine("Матриця:");
+        matrix.PrintMatrix();
+
+        Console.WriteLine("\nГоловна діагональ: " + matrix[0]);
+        Console.WriteLine("Побічна діагональ: " + matrix[1]);
+
+        Console.WriteLine($"\nКількість голосних у масиві: {matrix.VowelCount}");
+    }
+}
